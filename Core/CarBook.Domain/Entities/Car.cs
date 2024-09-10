9d593f7e1dee415fde56f92194d7dbc366fc017f
cang8 <0.gizemgunes@gmail.com>
2024-09-10 19:03:03 +0300

    Add project files.

---
+++ Core/CarBook.Domain/Entities/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+        public string Model { get; set; }
+    }
+}
