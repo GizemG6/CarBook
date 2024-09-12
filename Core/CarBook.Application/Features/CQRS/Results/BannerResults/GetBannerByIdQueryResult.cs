5aba6950dd116d54aa3e2a9a474a2c2306073f4e
cang8 <0.gizemgunes@gmail.com>
2024-09-12 21:00:25 +0300

    added-banner-cqrs

---
+++ Core/CarBook.Application/Features/CQRS/Results/BannerResults/GetBannerByIdQueryResult.cs
@@ -0,0 +1,18 @@
+﻿using CarBook.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.CQRS.Results.BannerResults
+{
+    public class GetBannerByIdQueryResult
+    {
+        public int BannerId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string VideoDescription { get; set; }
+        public string VideoUrl { get; set; }
+    }
+}
