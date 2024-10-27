936905957fc781a0ff694ef5638106088e60e89b
cang8 <0.gizemgunes@gmail.com>
2024-10-27 22:36:21 +0300

    added-contactcontroller-and-footerAddressComponentPartial

---
+++ Frontends/CarBook.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using CarBook.Dto.FooterAddressDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
