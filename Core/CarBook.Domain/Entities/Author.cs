fcf8002858807bb523fb3250dbb8be448f66c07f
cang8 <0.gizemgunes@gmail.com>
2024-11-23 23:16:41 +0300

    added-blog-author-entities

--- Core/CarBook.Domain/Entities/Author.cs
+++ Core/CarBook.Domain/Entities/Author.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace CarBook.Domain.Entities
 {
-    public class Category
+    public class Author
     {
-        public int CategoryId { get; set; }
+        public int AuthorID { get; set; }
         public string Name { get; set; }
-        public List<Blog> Blogs { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
     }
 }
