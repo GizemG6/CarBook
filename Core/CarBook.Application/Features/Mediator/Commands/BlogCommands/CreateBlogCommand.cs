dd593cac6f6b605fd2224e2ba8c07ffb7c9af67d
cang8 <0.gizemgunes@gmail.com>
2024-11-24 22:01:23 +0300

    updated-authorid

--- Core/CarBook.Application/Features/Mediator/Commands/BlogCommands/CreateBlogCommand.cs
+++ Core/CarBook.Application/Features/Mediator/Commands/BlogCommands/CreateBlogCommand.cs
@@ -11,7 +11,7 @@ namespace CarBook.Application.Features.Mediator.Commands.BlogCommands
     public class CreateBlogCommand : IRequest
     {
         public string Title { get; set; }
-        public string AuthorID { get; set; }
+        public int AuthorID { get; set; }
         public string CoverImageUrl { get; set; }
         public DateTime CreatedDate { get; set; }
         public int CategoryID { get; set; }
--- Core/CarBook.Domain/Entities/Blog.cs
+++ Core/CarBook.Domain/Entities/Blog.cs
@@ -10,7 +10,7 @@ namespace CarBook.Domain.Entities
     {
         public int BlogId { get; set; }
         public string Title { get; set; }
-        public string AuthorID { get; set; }
+        public int AuthorID { get; set; }
         public Author Author { get; set; }
         public string CoverImageUrl { get; set; }
         public DateTime CreatedDate { get; set; }
