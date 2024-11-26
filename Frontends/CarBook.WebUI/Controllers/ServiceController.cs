e2d6a7d87cd09064e83bd2c19d2df72a19c96ece
cang8 <0.gizemgunes@gmail.com>
2024-11-26 13:59:13 +0300

    edited-navbarUILayout(added-ServiceIndex-and-CarIndex)

--- Frontends/CarBook.WebUI/Controllers/ServiceController.cs
+++ Frontends/CarBook.WebUI/Controllers/ServiceController.cs
@@ -7,7 +7,9 @@ namespace CarBook.WebUI.Controllers
     public class ServiceController : Controller
     {
         public IActionResult Index()
-        { 
+        {
+            ViewBag.v1 = "Hizmetler";
+            ViewBag.v2 = "Hizmetlerimiz";
             return View();
         }
     }
