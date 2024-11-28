bd8e03610cac98a894cdcbbe683058a6eefeca42
cang8 <0.gizemgunes@gmail.com>
2024-11-28 23:43:12 +0300

    fixed-BlogDetailsTagCloudComponentPartial

--- Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsTagCloudComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsTagCloudComponentPartial.cs
@@ -4,19 +4,20 @@ using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.BlogViewComponents
 {
-    public class _BlogDetailsTagCloudByBlogComponentPartial : ViewComponent
+    public class _BlogDetailsTagCloudComponentPartial : ViewComponent
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public _BlogDetailsTagCloudByBlogComponentPartial(IHttpClientFactory httpClientFactory)
+        public _BlogDetailsTagCloudComponentPartial(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
 
         public async Task<IViewComponentResult> InvokeAsync(int id)
         {
+            ViewBag.blogId = id;
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"https://localhost:7194/api/TagClouds/" + id);
+            var responseMessage = await client.GetAsync($"https://localhost:7194/api/TagClouds/GetTagCloudByBlogId?id=" + id);
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
