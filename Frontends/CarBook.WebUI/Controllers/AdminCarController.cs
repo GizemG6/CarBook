06a21b77c04a391d3768823e22f749312734c56f
cang8 <0.gizemgunes@gmail.com>
2024-12-01 17:27:00 +0300

    added-UpdateCar-on-AdminCarController

--- Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -72,5 +72,19 @@ namespace CarBook.WebUI.Controllers
 
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateCar(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7194/api/Cars/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<UpdateCarDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
     }
 }
