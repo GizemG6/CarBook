e67c64314bbe3e435048e6914bc202c7d204b436
cang8 <0.gizemgunes@gmail.com>
2024-12-02 00:12:30 +0300

    added-CreateFeatureDto-and-UpdateFeatureDto

--- Presentation/CarBook.WebApi/Controllers/FeaturesController.cs
+++ Presentation/CarBook.WebApi/Controllers/FeaturesController.cs
@@ -36,7 +36,7 @@ namespace CarBook.WebApi.Controllers
             await _mediator.Send(command);
             return Ok("Özellik eklendi");
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> RemoveFeature(int id)
         {
             await _mediator.Send(new RemoveFeatureCommand(id));
