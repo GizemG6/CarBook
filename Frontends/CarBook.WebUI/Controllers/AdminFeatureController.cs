440c92ffeffbf98e4cf1b745b4a06f81570fa25a
cang8 <0.gizemgunes@gmail.com>
2024-12-02 00:38:32 +0300

    added-CreateFeaturePage-and-UpdateFeaturePage

--- Frontends/CarBook.WebUI/Controllers/AdminFeatureController.cs
+++ Frontends/CarBook.WebUI/Controllers/AdminFeatureController.cs
@@ -67,7 +67,7 @@ namespace CarBook.WebUI.Controllers
         public async Task<IActionResult> UpdateFeature(int id)
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"https://localhost:7194/api/Brands/{id}");
+            var responseMessage = await client.GetAsync($"https://localhost:7194/api/Features/{id}");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
