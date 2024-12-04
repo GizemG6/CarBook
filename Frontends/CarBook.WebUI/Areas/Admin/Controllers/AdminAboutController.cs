8011d491635c3d82e1d012a5313c934f0aeaa72f
cang8 <0.gizemgunes@gmail.com>
2024-12-04 14:28:57 +0300

    added-UpdateAboutPage-on-AdminArea

--- Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminAboutController.cs
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminAboutController.cs
@@ -1,4 +1,5 @@
 ﻿using CarBook.Dto.AboutDtos;
+using CarBook.Dto.BannerDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Text;
@@ -61,6 +62,38 @@ namespace CarBook.WebUI.Areas.Admin.Controllers
             {
                 return RedirectToAction("Index", "AdminAbout", new { area = "Admin" });
             }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("UpdateAbout/{id}")]
+
+        public async Task<IActionResult> UpdateAbout(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7194/api/Abouts/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateAboutDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [Route("UpdateAbout/{id}")]
+        public async Task<IActionResult> UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateAboutDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7194/api/Abouts", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminAbout", new { area = "Admin" });
+            }
+
             return View();
         }
     }
