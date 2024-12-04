f9fbbfd01fe4113234936670fc61397c5d7ae0c6
cang8 <0.gizemgunes@gmail.com>
2024-12-04 12:40:58 +0300

    added-UpdateBannerPage

--- Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
@@ -66,5 +66,36 @@ namespace CarBook.WebUI.Areas.Admin.Controllers
 
             return View();
         }
+
+        [HttpGet]
+        [Route("UpdateBanner/{id}")]
+
+        public async Task<IActionResult> UpdateBanner(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7194/api/Brands/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateBannerDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateBanner(UpdateBannerDto updateBannerDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateBannerDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7194/api/Banners", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
     }
 }
