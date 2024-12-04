26748aa2e14211b1979aa159358d9cddde184ea1
cang8 <0.gizemgunes@gmail.com>
2024-12-04 13:32:38 +0300

    added-CreateAboutDto-and-UpdateAboutDto

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminAboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    public class AdminAboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
