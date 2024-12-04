bdab8036b7c6847908ed677cca8aad588432a5bf
cang8 <0.gizemgunes@gmail.com>
2024-12-04 15:05:23 +0300

    added-UpdateAuthorPage-on-AdminArea

--- Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminAuthorController.cs
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminAuthorController.cs
@@ -52,5 +52,49 @@ namespace CarBook.WebUI.Areas.Admin.Controllers
             }
             return View();
         }
+
+        [Route("RemoveAuthor/{id}")]
+        public async Task<IActionResult> RemoveAuthor(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7194/api/Authors?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminAuthor", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("UpdateAuthor/{id}")]
+
+        public async Task<IActionResult> UpdateAuthor(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7194/api/Authors/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateAuthorDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [Route("UpdateAuthor/{id}")]
+        public async Task<IActionResult> UpdateAuthor(UpdateAuthorDto updateAuthorDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateAuthorDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7194/api/Authors", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminAuthor", new { area = "Admin" });
+            }
+
+            return View();
+        }
     }
 }
