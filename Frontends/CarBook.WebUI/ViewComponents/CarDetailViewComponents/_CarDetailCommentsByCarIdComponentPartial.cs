979eb9a799c32c54f90bd1e052a48d7516dbc942
cang8 <0.gizemgunes@gmail.com>
2024-12-10 16:23:04 +0300

    added-Review-Entity

--- Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailCommentsByCarIdComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailCommentsByCarIdComponentPartial.cs
@@ -11,18 +11,6 @@ namespace CarBook.WebUI.ViewComponents.CarDetailViewComponents
 			_httpClientFactory = httpClientFactory;
 		}
 
-		public async Task<IViewComponentResult> InvokeAsync(int id)
-		{
-			ViewBag.carid = id;
-			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.GetAsync("https://localhost:7194/api/Reviews?id=" + id);
-			if (responseMessage.IsSuccessStatusCode)
-			{
-				var jsonData = await responseMessage.Content.ReadAsStringAsync();
-				var values = JsonConvert.DeserializeObject<List<ResultReviewByCarIdDto>>(jsonData);
-				return View(values);
-			}
-			return View();
-		}
+		
 	}
 }
