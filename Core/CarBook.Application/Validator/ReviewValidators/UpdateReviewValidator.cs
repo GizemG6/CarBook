5f33722cd84248105bec55f3090546de15c4fa4a
cang8 <0.gizemgunes@gmail.com>
2024-12-10 16:38:24 +0300

    added-validator-for-review

--- Core/CarBook.Application/Validator/ReviewValidators/UpdateReviewValidator.cs
+++ Core/CarBook.Application/Validator/ReviewValidators/UpdateReviewValidator.cs
@@ -8,9 +8,9 @@ using System.Threading.Tasks;
 
 namespace CarBook.Application.Validator.ReviewValidators
 {
-    public class CreateReviewValidator : AbstractValidator<CreateReviewCommand>
+    public class UpdateReviewValidator : AbstractValidator<UpdateReviewCommand>
     {
-        public CreateReviewValidator()
+        public UpdateReviewValidator()
         {
             RuleFor(x => x.CustomerName).NotEmpty().WithMessage("Lütfen müşteri adını boş geçmeyiniz!");
             RuleFor(x => x.CustomerName).MinimumLength(5).WithMessage("Lütfen en az 5 karakter veri girişi yapınız!");
@@ -18,6 +18,7 @@ namespace CarBook.Application.Validator.ReviewValidators
             RuleFor(x => x.Comment).NotEmpty().WithMessage("Lütfen yorum değerini boş geçmeyiniz");
             RuleFor(x => x.Comment).MinimumLength(50).WithMessage("Lütfen yorum kısmına en az 50 karakter veri girişi yapınız");
             RuleFor(x => x.Comment).MaximumLength(500).WithMessage("Lütfen yorum kısmına en fazla 500 karakter veri girişi yapınız");
+            RuleFor(x => x.CustomerImage).NotEmpty().WithMessage("Lütfeen müşteri görselini boş geçmeyiniz!").MinimumLength(10).WithMessage("Lütfen en az 10 karakter uzunluğunda veri girişi yapınız!").MaximumLength(200).WithMessage("Lütfen en fazla 200 karakter uzunluğunda veri girişi yapınız!");
         }
     }
 }
